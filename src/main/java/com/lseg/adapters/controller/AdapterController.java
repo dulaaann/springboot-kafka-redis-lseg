58431390b12a2da0c50282c0fbd50d5af1cab441
dulanherath <dulanmherath@outlook.com>
2024-01-18 23:22:20 +0530

    implementation is done with kafka consumer

--- src/main/java/com/lseg/adapters/controller/AdapterController.java
+++ src/main/java/com/lseg/adapters/controller/AdapterController.java
@@ -4,6 +4,7 @@ import com.lseg.adapters.dto.DataDto;
 import com.lseg.adapters.service.DataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,12 +15,19 @@ public class AdapterController<T> {
     @Autowired
     private DataService dataService;
 
-    @PostMapping("/process")
-    public ResponseEntity<String> processAndSave(@RequestBody DataDto<T> dto) {
+//    @PostMapping("/process")
+//    public ResponseEntity<String> processAndSave(@RequestBody DataDto<T> dto) {
+//        dataService.processAndSaveToRedis(dto);
+//        dataService.processAndSaveToPostgres(dto);
+//        return ResponseEntity.ok("Data processed and saved successfully");
+//    }
+    @KafkaListener(topics = "given-topic",groupId = "given-group")
+    public ResponseEntity<String> processAndSave(@RequestBody DataDto<T> dto){
         dataService.processAndSaveToRedis(dto);
         dataService.processAndSaveToPostgres(dto);
         return ResponseEntity.ok("Data processed and saved successfully");
     }
+
 
     @GetMapping("/retrieve/{id}")
     public ResponseEntity<T> retrieveDataFromRedis(@PathVariable Long id) {
